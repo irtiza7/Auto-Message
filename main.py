14caecb0380a15cd30f340601553e29a89796eb1
Muhammad Irtiza <m7.irtiza@gmail.com>
2022-08-08 00:23:54 +0500

    Added functionality to take parameter values from the user, and to send message on a specific date.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+"""
+Idea: The application sends a message on WhatsApp.
+"""
+from datetime import date
+import time
 import pywhatkit as pywt
 
-contact = input("Enter the contact: ")
-message = input("Enter the message: ")
-hour = input("Enter the hour: ")
-minutes = input("Enter the minutes: ")
-day = input("Enter the day: ")
-wait_sec = input("After how many seconds to send the message: ")
+contact = "+923090047457"  # input("Enter the contact: ")
+message = "Automatic Message."  # input("Enter the message: ")
+tragetted_hour = int(input("Enter hour (in 24-hour format): "))
+tragetted_minutes = int(input("Enter minutes: "))
+targetted_date = int(input("Enter date (01 to 31): "))
+wait_sec = int(input("After how many seconds (15 to onwards) to send the message: "))
 
-pywt.sendwhatmsg(contact, message, 19, 12)
+while True:
+    today_date = date.today()
+    if targetted_date == int(today_date.strftime("%d")):
+        break
+
+    time.sleep(1)
+
+pywt.sendwhatmsg(contact, message, tragetted_hour, tragetted_minutes, wait_sec)
